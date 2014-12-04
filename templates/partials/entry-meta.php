2574cea8b90d91f62b221f02474141bc28bc6c06
Magda Faizov <magda@dxw.com>
2014-12-04 11:58:43 +0000

    updated icon font and date styles

--- templates/partials/entry-meta.php
+++ templates/partials/entry-meta.php
@@ -1,5 +1,5 @@
 <ul class="entry-meta">
   <li>
-    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><span><?php the_time('j'); ?></span> <?php echo the_time('F Y'); ?></time>
   </li>
 </ul>
