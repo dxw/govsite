d60c2542ba6e6d9ad51da1294281757c945ae68a
Magda Faizov <magda@dxw.com>
2014-12-19 17:36:04 +0000

    story 84585844, private posts and password protected posts in homepage loop

--- templates/page-home.php
+++ templates/page-home.php
@@ -106,6 +106,23 @@ the_post();
                   <?php the_excerpt(); ?>
                 </article>
 
+              <?php } elseif ( get_post_status ( $ID ) == 'private' || post_password_required() ) { ?>
+                
+                <article <?php post_class('summary'); ?>>
+                  <header>
+                    <span class="icon"></span>
+                    <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                  </header>
+
+                  <?php get_template_part('partials/entry-meta'); ?>
+
+                  <?php if ( has_post_thumbnail() ) { ?>
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+                  <?php } ?>
+
+                  <?php the_excerpt(); ?>
+                </article>
+
               <?php } else { ?>
                 
                 <article <?php post_class('summary'); ?>>
