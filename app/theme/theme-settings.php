4d666e3ec5a0862a1726248136ce3d09ebd980ac
Tom Adams <tom@dxw.com>
2015-01-08 13:33:38 -0500

    Replace strings in add_action with functions

--- app/theme/theme-settings.php
+++ app/theme/theme-settings.php
@@ -1,16 +1,12 @@
 <?php 
 
-add_action( 'admin_menu', 'my_admin_menu' );
-
-function my_admin_menu() {
+add_action( 'admin_menu', function () {
   add_theme_page( 'Logo', 'Logo', 'manage_options', 'logo', 'logo_options' );
   add_theme_page( 'Social media', 'Social media', 'manage_options', 'social-media', 'social_media_options' );
   add_theme_page( 'Footer link', 'Footer link', 'manage_options', 'footer-link', 'footer_link_options' );
-}
+});
 
-add_action( 'admin_init', 'my_admin_init' );
-
-function my_admin_init() {
+add_action( 'admin_init', function () {
 
   add_settings_section( 'header', 'Instructions', 'header_logo_callback', 'logo' );
   register_setting( 'logo', 'logo-setting' );
@@ -31,7 +27,7 @@ function my_admin_init() {
   add_settings_field( 'footer-cta', 'Footer call to action', 'footer_cta_callback', 'footer-link', 'header' );
   add_settings_field( 'footer-url', 'Footer URL', 'footer_url_callback', 'footer-link', 'header' );
 
-}
+});
 
 // Logo
 function header_logo_callback() {
