6ab2a2cdf5d99f0113c2d8f5e86f89e0e90ee872
Tom Adams <tom@dxw.com>
2015-01-12 11:36:07 -0500

    Only show sticky posts on the first page of results

--- templates/archive.php
+++ templates/archive.php
@@ -1,3 +1,7 @@
+<?php
+global $wp_query;
+?>
+
 <div class="row">
 
   <div class="large-12 columns">
@@ -15,19 +19,20 @@
 
       <div class="medium-8 large-8 columns">
 
-        <?php
-        global $wp_query;
-        $q = $wp_query->query;
-        $q['ignore_sticky_posts'] = true;
-        $q['post__in'] = get_option('sticky_posts');
-        $q['posts_per_page'] = -1;
-        query_posts($q);
-        ?>
+        <?php if (get_query_var('paged') < 2) : ?>
+          <?php
+          $q = $wp_query->query;
+          $q['ignore_sticky_posts'] = true;
+          $q['post__in'] = get_option('sticky_posts');
+          $q['posts_per_page'] = -1;
+          query_posts($q);
+          ?>
 
-        <?php while (have_posts()) : ?>
-          <?php the_post() ?>
-          <?php get_template_part('partials/article-list-item') ?>
-        <?php endwhile; ?>
+          <?php while (have_posts()) : ?>
+            <?php the_post() ?>
+            <?php get_template_part('partials/article-list-item') ?>
+          <?php endwhile; ?>
+        <?php endif ?>
 
         <?php
         wp_reset_query();
