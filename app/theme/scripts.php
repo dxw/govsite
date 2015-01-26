c0d54fd342d28a9f3c5f51486cd8706e9fae83bf
Magda Faizov <m.maciaszczyk@gmail.com>
2015-01-26 17:09:35 +0000

    use main and child theme styles on govsite-premium

--- app/theme/scripts.php
+++ app/theme/scripts.php
@@ -11,23 +11,20 @@ add_action('wp_enqueue_scripts', function () {
 
   // We need to register our own jQuery, because WP is on jQuery 2.x (no it isn't) which breaks support for IE 6-8.
   // This will not affect admin pages
-  // This will break any plugin that requires a feature/behaviour in jQuery 2.x which is missing/different in jQuery 1.10.x
-
-  $u = get_stylesheet_directory_uri();
-  $uri = substr($u, 0, strlen($u)-strlen("/templates")); 
+  // This will break any plugin that requires a feature/behaviour in jQuery 2.x which is missing/different in jQuery 1.10.x 
 
   wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', $uri . '/build/lib/jquery.min.js');
+  wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/../build/lib/jquery.min.js');
 
   // Because it's awesome
-  wp_enqueue_script('modernizr', $uri . '/build/lib/modernizr.min.js');
+  wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/../build/lib/modernizr.min.js');
 
   // Pretty much everything else should be compiled by Grunt.
-  wp_enqueue_script('main', $uri . '/build/main.min.js', array('jquery', 'modernizr'), '', true);
+  wp_enqueue_script('main', get_stylesheet_directory_uri() . '/../build/main.min.js', array('jquery', 'modernizr'), '', true);
 
-  wp_enqueue_style ('main', $uri . '/build/main.min.css');
+  wp_enqueue_style ('main', get_stylesheet_directory_uri() . '/../build/main.min.css');
 });
 
 add_action('admin_enqueue_scripts', function () {
-  wp_enqueue_script('admin',      get_template_directory_uri() . '/../assets/js/admin.js', ['jquery'], '', true);
+  wp_enqueue_script('admin', get_template_directory_uri() . '/../assets/js/admin.js', ['jquery'], '', true);
 });
