76ced316d59a2efbae231d19a5336b8af341d5b4
Duncan Stuart <dgmstuart@gmail.com>
2015-01-27 16:01:13 +0000

    Featured images can now be uploaded - fixes pivotal bug 86320098. The source issue seems to be the uploading of wp_enqueue_media: this can't be done in admin_init - that's too early: http://codex.wordpress.org/Function_Reference/wp_enqueue_media

--- app/theme/logo.php
+++ app/theme/logo.php
@@ -11,7 +11,10 @@ add_action('admin_init', function () {
   add_settings_field('logo-image', 'Logo image', function () {
     echo '<input type="text" name="logo-setting" value="'.esc_attr(get_option('logo-setting')).'" size="50">';
   }, 'logo', 'header');
+});
 
+add_action('admin_enqueue_scripts', function () {
   // Add media scripts
+  // N.B. this can't be done in admin_init - that's too early: http://codex.wordpress.org/Function_Reference/wp_enqueue_media
   wp_enqueue_media();
 });
