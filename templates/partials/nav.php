48be994abc2dbbd76bcd30408d5433e00fe7c497
Alex Sexton <alex@dxw.com>
2015-01-30 15:00:23 +0000

    moved custom nav functions from /partials/nav.php to theme/menus.php

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,50 +1,6 @@
 <nav id="headermenu" class="headermenu top-bar" data-topbar role="navigation">
 
   <button type="button" class="button-search show-for-medium-up"><span class="accessibility">Search</span></button>
-  
-  <?php
-  //Deletes all CSS classes and id's, except for those listed in the array below
-  //Modified from https://gist.github.com/lekkerduidelijk/5576437
-  function custom_wp_nav_menu($var) {
-    return is_array($var) ? array_intersect($var, array(
-      //List of allowed menu classes
-      'current_page_item',
-      'current_page_parent',
-      'current_page_ancestor',
-      'first',
-      'last',
-      'vertical',
-      'horizontal',
-      'menu-item-has-children',
-      'sub-menu'
-      )
-    ) : '';
-  }
-  add_filter('nav_menu_css_class', 'custom_wp_nav_menu');
-  add_filter('nav_menu_item_id', 'custom_wp_nav_menu');
-  add_filter('page_css_class', 'custom_wp_nav_menu');
-
-  //Replaces "current-menu-item" with "active"
-  function current_to_active($text){
-    $replace = array(
-      //List of menu item classes that should be changed to "active"
-      'current_page_item' => 'active',
-      'current_page_parent' => 'active',
-      'current_page_ancestor' => 'active',
-      'menu-item-has-children' => 'has-dropdown',
-      'sub-menu' => 'dropdown',
-    );
-    $text = str_replace(array_keys($replace), $replace, $text);
-      return $text;
-    }
-  add_filter ('wp_nav_menu','current_to_active');
-
-  //Deletes empty classes
-  function strip_empty_classes($menu) {
-      return $menu;
-  }
-  add_filter ('wp_nav_menu','strip_empty_classes');
-  ?>
 
   <?php
   //Add headernav with arguments and a nice clean class structure yay!
