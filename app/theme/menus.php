862ccbbfa610f0bec07c76047588a6f3bb171e9c
Magda Faizov <magda@dxw.com>
2015-03-10 17:11:01 +0000

    show active menu item for category page

--- app/theme/menus.php
+++ app/theme/menus.php
@@ -11,6 +11,7 @@ function custom_wp_nav_menu($var) {
     'current_page_item',
     'current_page_parent',
     'current_page_ancestor',
+    'current-menu-item',
     'first',
     'last',
     'vertical',
@@ -32,6 +33,7 @@ function current_to_active($text){
     'current_page_item' => 'active',
     'current_page_parent' => 'active',
     'current_page_ancestor' => 'active',
+    'current-menu-item' => 'active',
     'menu-item-has-children' => 'has-dropdown',
     'sub-menu' => 'dropdown',
   );
