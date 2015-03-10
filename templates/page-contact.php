20b08cd09101a0c38bf44f981fa45104113c52cc
Magda Faizov <magda@dxw.com>
2015-03-10 16:02:34 +0000

    fixed IE7 bugs

--- templates/page-contact.php
+++ templates/page-contact.php
@@ -22,38 +22,42 @@ the_post();
       <h1><?php the_title(); ?></h1>
       </header>
 
-      <div class="medium-8 columns">
-        <article class="rte">
-          <?php the_content(); ?>
-        </article>
+      <div class="row">
+
+        <div class="medium-8 columns">
+          <article class="rte">
+            <?php the_content(); ?>
+          </article>
+        </div>
+
+        <aside class="sidebar medium-4 columns" role="complementary">
+
+        <?php if ( get_field('phone_number') || get_field('email_address') || get_field('address') ): ?>
+
+        <div class="address-panel">
+          <header>
+          <h3>Contact details</h3>
+          </header>
+          
+          <?php if ( get_field('address') ) : ?>
+          <address><?php the_field('address'); ?></address>
+          <?php endif ?>
+
+          <?php if ( get_field('phone_number') ) : ?>
+          <a class="contact-link phone"><?php the_field('phone_number'); ?></a>
+          <?php endif ?>
+          
+          <?php if ( get_field('email_address') ) : ?>
+          <a class="contact-link email"><?php the_field('email_address'); ?></a>
+          <?php endif ?>
+
+        </div>
+
+        <?php endif ?>
+
+        </aside>
+
       </div>
-
-      <aside class="sidebar medium-4 columns" role="complementary">
-
-      <?php if ( get_field('phone_number') || get_field('email_address') || get_field('address') ): ?>
-
-      <div class="address-panel">
-        <header>
-        <h3>Contact details</h3>
-        </header>
-        
-        <?php if ( get_field('address') ) : ?>
-        <address><?php the_field('address'); ?></address>
-        <?php endif ?>
-
-        <?php if ( get_field('phone_number') ) : ?>
-        <a class="contact-link phone"><?php the_field('phone_number'); ?></a>
-        <?php endif ?>
-        
-        <?php if ( get_field('email_address') ) : ?>
-        <a class="contact-link email"><?php the_field('email_address'); ?></a>
-        <?php endif ?>
-
-      </div>
-
-      <?php endif ?>
-
-      </aside>
 
       </div>
 
