7946f59527e5f7ecbe5af0836a33d56aaea11a0e
Magda Faizov <magda@dxw.com>
2015-03-12 15:05:06 +0000

    comment styles

--- templates/partials/comment.php
+++ templates/partials/comment.php
@@ -1,21 +1,17 @@
-<div class="comment-author vcard">
-  <?php echo get_avatar($comment, $size = 32) ?>
-  <span class="fn"><?php echo get_comment_author_link() ?></span>
-</div>
-
 <div class="comment-meta">
+  <span class="comment-author"><?php echo get_avatar($comment, $size = 32) ?><?php echo get_comment_author_link() ?></span>
   <time datetime="<?php echo get_comment_date('c') ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)) ?>"><?php printf(__('%1$s at %2$s', 'roots'), get_comment_date(),  get_comment_time()) ?></a></time>
-  <?php edit_comment_link(__('(Edit)', 'roots'), '', '') ?>
 </div>
 
 <?php if ($comment->comment_approved == '0') : ?>
-  <div class="alert-box">
+  <div class="alert-box" role="alert">
     <?php _e('Your comment is awaiting moderation.', 'roots') ?>
   </div>
 <?php endif ?>
 
 <div class="comment-text">
   <?php comment_text() ?>
+  <?php edit_comment_link(__('(Edit comment)', 'roots'), '', '') ?>
 </div>
 
 <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
