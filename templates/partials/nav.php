3063ce8257ffd54376d58833a12d3dc809117b93
Magda Faizov <m.maciaszczyk@gmail.com>
2015-03-13 17:16:49 +0000

    IE search form

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -12,12 +12,10 @@
   ?>
 </nav>
 
-<!--[if lt IE 8]>
-  <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url('/'); ?>">
-    <div>
-      <label class="screen-reader-text hidden-for-small-only hidden-for-medium-up" for="s">Search for:</label>
-      <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s">
-      <input type="submit" id="searchsubmit" value="Search">
-    </div>
-  </form>
-<![endif]-->
+<form role="search" method="get" id="searchform" class="searchform ie-visible" action="<?php echo home_url('/'); ?>">
+  <div>
+    <label class="screen-reader-text hidden-for-small-only hidden-for-medium-up" for="s">Search for:</label>
+    <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s">
+    <input type="submit" id="searchsubmit" value="Search">
+  </div>
+</form>
