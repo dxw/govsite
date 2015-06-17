accefbc76dcf3361b01f1f887ca395937e3d52f1
Adam Onishi <adam@dxw.com>
2015-06-17 11:53:56 +0100

    refactor(templates): I18n on search text and whitespace

--- templates/search.php
+++ templates/search.php
@@ -5,17 +5,17 @@
       <div class="large-9 columns">
 
         <header class="page-header">
-          <h1>You have searched for <strong><?php the_search_query(); ?></strong></h1>
+          <h1><?php printf( __('You have searched for <strong>%s</strong>', 'govsite'), get_search_query() ) ?></h1>
         </header>
 
         <div class="page-element">
           <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-              <?php get_template_part('partials/search-item'); ?>
-            <?php endwhile; ?>
+            <?php while (have_posts()) : the_post() ?>
+              <?php get_template_part('partials/search-item') ?>
+            <?php endwhile ?>
           <?php else: ?>
-            <p>No results found. Search again?</p>
-          <?php endif; ?>
+            <p><?php _e('No results found. Search again?', 'govsite') ?></p>
+          <?php endif ?>
         </div>
 
         <?php get_template_part('partials/pager') ?>
