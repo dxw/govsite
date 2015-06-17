90cdef73346d769380a9379cc4f40087d46e2b7c
Adam Onishi <adam@dxw.com>
2015-06-17 11:59:40 +0100

    refactor(templates): Remove those pesky semicolons

--- templates/home.php
+++ templates/home.php
@@ -4,16 +4,16 @@
 
       <header class="large-12 column">
         <div class="header-group">
-          <h1><?php echo bloginfo('name'); ?></h1>
-          <h2><?php echo bloginfo('description'); ?></h2>
+          <h1><?php echo bloginfo('name') ?></h1>
+          <h2><?php echo bloginfo('description') ?></h2>
         </div>
       </header>
 
       <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-          <?php get_template_part('partials/article-list-item'); ?>
-        <?php endwhile; ?>
-      <?php endif; ?>
+        <?php while (have_posts()) : the_post() ?>
+          <?php get_template_part('partials/article-list-item') ?>
+        <?php endwhile ?>
+      <?php endif ?>
 
       <div class="large-12 column">
         <?php get_template_part('partials/pager') ?>
