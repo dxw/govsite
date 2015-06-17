275c8c7797e22dc965156def0020c3b4b2a561d9
Adam Onishi <adam@dxw.com>
2015-06-17 11:29:05 +0100

    refactor(templates): Internationalise and fix whitespace inconsistencies

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7 ie6" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8 ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]><html class="no-js lt-ie9 ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7 ie6" <?php language_attributes() ?>> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8 ie7" <?php language_attributes() ?>> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9 ie8" <?php language_attributes() ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes() ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+  <title><?php wp_title('|', true, 'right') ?><?php bloginfo('name') ?></title>
   <link rel="dns-prefetch" href="//maps.gstatic.com">
   <link rel="dns-prefetch" href="//www.google-analytics.com">
   <link rel="dns-prefetch" href="//fonts.googleapis.com">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php $favicon_url = get_attachment_url_by_slug('favicon'); ?>
+  <?php $favicon_url = get_attachment_url_by_slug('favicon') ?>
   <link rel="icon" type="image/png" href="<?php echo $favicon_url ?>" />
-  <?php wp_head(); ?>
+  <?php wp_head() ?>
   <!--[if lt IE 9]>
     <script src="//code.jquery.com/jquery-1.9.0.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/../assets/js/ie/browser-support.js"></script>
   <![endif]-->
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class() ?>>
   <!--[if lt IE 8]>
     <div class="alert-box alert">You are using an <strong>outdated</strong> browser. Please <a href="//browsehappy.com/">upgrade your browser</a> to improve your experience.</div>
   <![endif]-->
@@ -27,32 +27,32 @@
   <header class="site-header" role="banner">
     <div class="row">
       <div class="small-12 medium-3 columns">
-        <?php $logo = get_option('logo-setting'); ?>
-        <a href="/" title="<?php bloginfo('name'); ?>" class="logo"><img src="<?php echo $logo; ?>" alt="<?php bloginfo('name'); ?> logo"></a>
+        <?php $logo = get_option('logo-setting') ?>
+        <a href="<?php echo home_url('/') ?>" title="<?php bloginfo('name') ?>" class="logo"><img src="<?php echo $logo ?>" alt="<?php bloginfo('name') ?> logo"></a>
         <div class="toggle-container hidden-for-medium-up">
-          <a href="#top-bar" class="nav-toggle"><span class="visually-hidden">Menu</span></a>
+          <a href="#top-bar" class="nav-toggle"><span class="visually-hidden"><?php _e('Menu', 'govsite') ?></span></a>
         </div>
       </div>
       <div class="small-12 medium-9 columns">
-        <?php get_template_part('partials/nav'); ?>
+        <?php get_template_part('partials/nav') ?>
       </div>
     </div>
   </header>
 
-  <?php get_template_part('partials/header-search'); ?>
+  <?php get_template_part('partials/header-search') ?>
 
-  <?php w_requested_template(); ?>
+  <?php w_requested_template() ?>
 
   <footer class="site-footer" role="contentinfo">
 
     <div class="navigation">
       <div class="row">
         <div class="medium-6 columns">
-          <?php get_template_part('partials/nav-footer'); ?>
+          <?php get_template_part('partials/nav-footer') ?>
         </div>
         <div class="medium-6 columns">
           <?php if ( get_option('footer-link-text-setting') ) : ?>
-            <p><?php echo get_option('footer-link-text-setting'); ?> <a href="<?php echo get_option('footer-link-url-setting'); ?>"><?php echo get_option('footer-link-cta-setting'); ?></a></p>
+            <p><?php echo get_option('footer-link-text-setting') ?> <a href="<?php echo get_option('footer-link-url-setting') ?>"><?php echo get_option('footer-link-cta-setting') ?></a></p>
           <?php endif ?>
         </div>
       </div>
@@ -61,17 +61,17 @@
     <div class="credits">
       <div class="row">
         <div class="medium-6 columns">
-          <small>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> copyright</small>
+          <small>&copy; <?php echo date('Y') ?> <?php bloginfo('name') ?> copyright</small>
         </div>
         <div class="medium-6 columns show-for-medium-up">
-          <?php get_template_part('partials/social-media'); ?>
+          <?php get_template_part('partials/social-media') ?>
         </div>
       </div>
     </div>
 
   </footer>
 
-  <?php wp_footer(); ?>
+  <?php wp_footer() ?>
 
 </body>
 </html>
