2485a902535542b8d2b22e6bcf4c510944df1b07
Adam Onishi <adam@dxw.com>
2015-06-17 14:02:34 +0100

    refactor(home): Update grid wrappers, tidy up whitespace

--- templates/page-home.php
+++ templates/page-home.php
@@ -7,66 +7,55 @@ the_post() ?>
 <main id="content" role="main" class="main">
 
   <?php if(get_the_content()) : ?>
-
     <?php get_template_part('partials/page', 'banner') ?>
-
   <?php endif ?>
 
-  <section class="image-features">
-    <div class="row">
-      <div class="large-12 columns">
-        <ul class="small-block-grid-1 medium-block-grid-3">
+  <section class="image-features row">
+    <ul class="small-block-grid-1 medium-block-grid-3">
 
-          <?php for($img=1; $img<=3; $img++): ?>
+      <?php for($img=1; $img<=3; $img++): ?>
 
-            <?php
-              // Pass the $img variable into the partial
-              set_query_var( 'img', $img );
-              get_template_part( 'partials/image', 'feature' );
-            ?>
+        <?php
+          // Pass the $img variable into the partial
+          set_query_var( 'img', $img );
+          get_template_part( 'partials/image', 'feature' );
+        ?>
 
-          <?php endfor; ?>
+      <?php endfor; ?>
 
-        </ul>
-      </div>
-    </div>
+    </ul>
   </section>
 
-  <section class="news-posts">
-    <div class="row">
+  <section class="news-posts row">
 
-      <div class="posts medium-8 columns">
-        <header>
-          <h2><?php _e('News', 'govsite') ?></h2>
-        </header>
+    <div class="posts medium-8 columns">
+      <header>
+        <h2><?php _e('News', 'govsite') ?></h2>
+      </header>
 
-        <?php $news = new WP_Query( array ('posts_per_page' => 4) ); ?>
+      <?php $news = new WP_Query( array ('posts_per_page' => 4) ); ?>
 
-        <?php if( $news->have_posts()) : while ($news->have_posts()) : $news->the_post() ?>
+      <?php if( $news->have_posts()) : while ($news->have_posts()) : $news->the_post() ?>
 
-          <?php if (is_sticky()) : ?>
-
-            <?php get_template_part('partials/featured-news-item') ?>
-
-          <?php else : ?>
-
-            <?php get_template_part('partials/news-item') ?>
-
-          <?php endif ?>
-        <?php endwhile; endif; ?>
-
-        <?php wp_reset_query() ?>
-
-        <?php if (get_field('news_page_url')) : ?>
-          <a href="<?php the_field('news_page_url') ?>" class="button"><?php _e('More news', 'govsite') ?></a>
+        <?php if (is_sticky()) : ?>
+          <?php get_template_part('partials/featured-news-item') ?>
+        <?php else : ?>
+          <?php get_template_part('partials/news-item') ?>
         <?php endif ?>
-      </div>
 
-      <?php if (get_field('banner_title')) : ?>
-        <?php get_template_part('partials/sidebar', 'banner') ?>
+      <?php endwhile; endif; ?>
+
+      <?php wp_reset_query() ?>
+
+      <?php if (get_field('news_page_url')) : ?>
+        <a href="<?php the_field('news_page_url') ?>" class="button"><?php _e('More news', 'govsite') ?></a>
       <?php endif ?>
-
     </div>
+
+    <?php if (get_field('banner_title')) : ?>
+      <?php get_template_part('partials/sidebar', 'banner') ?>
+    <?php endif ?>
+
   </section>
 
 </main>
