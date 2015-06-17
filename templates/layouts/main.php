8d9a9613146b5d1d56114878b5417fb46ab5467a
Adam Onishi <adam@dxw.com>
2015-06-17 17:08:27 +0100

    refactor(main): Move header search partial into main template and delete partial

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -39,7 +39,11 @@
     </div>
   </header>
 
-  <?php get_template_part('partials/header-search') ?>
+  <div class="header-search">
+    <div class="row">
+      <?php get_template_part('partials/search-form'); ?>
+    </div>
+  </div>
 
   <?php w_requested_template() ?>
 
