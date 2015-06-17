47f9ea1969fde885b16503d054e4ddb85bea1ac9
Adam Onishi <adam@dxw.com>
2015-06-17 11:18:03 +0100

    refactor(404): Internationalise 404 template

--- templates/404.php
+++ templates/404.php
@@ -5,24 +5,24 @@
 
       <header class="page-header">
         <div class="header-group">
-          <h1>Page not found</h1>
+          <h1><?php _e('Page not found', 'govsite') ?></h1>
         </div>
       </header>
 
       <div class="row">
         <div class="medium-8 columns">
-          <h4>Sorry, but the page you were trying to view does not exist.</h4>
+          <h4><?php _e('Sorry, but the page you were trying to view does not exist.', 'govsite') ?></h4>
           <ul>
-            <li>We might have removed the page.</li>
-            <li>The link you clicked might be old.</li>
-            <li>You might have accidentally typed the wrong URL in the address bar.</li>
+            <li><?php _e('We might have removed the page.', 'govsite') ?></li>
+            <li><?php _e('The link you clicked might be old.', 'govsite') ?></li>
+            <li><?php _e('You might have accidentally typed the wrong URL in the address bar.', 'govsite') ?></li>
           </ul>
-          <p>Try retyping the URL again  or use search in the header to find what you're looking for.</p>
-          <a href="/" class="button">Return to homepage</a>
+          <p><?php _e('Try retyping the URL again  or use search in the header to find what you\'re looking for.', 'govsite') ?></p>
+          <a href="<?php echo home_url( '/' ) ?>" class="button"><?php _e('Return to homepage', 'govsite') ?></a>
         </div>
 
         <aside class="medium-4 columns show-for-medium-up">
-          <span class="error-image">404</span>
+          <span class="error-image"><?php _e('404', 'govsite') ?></span>
         </aside>
 
       </div>
