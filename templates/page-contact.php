e9828970038297551c71a661fb3f25b10cd57971
Adam Onishi <adam@dxw.com>
2015-06-17 11:30:29 +0100

    refactor(contact): Fix whitespace and code style inconsistencies

--- templates/page-contact.php
+++ templates/page-contact.php
@@ -1,27 +1,26 @@
-<?php 
+<?php
 /* Template name: Contact page */
 
-the_post(); 
-
+the_post();
 ?>
 
 <main id="content" role="main" class="main">
 
-  <?php get_template_part('partials/map'); ?>
+  <?php get_template_part('partials/map') ?>
 
   <section class="contact-us">
     <div class="row">
       <div class="large-12 columns">
 
         <header class="page-header">
-          <h1><?php the_title(); ?></h1>
+          <h1><?php the_title() ?></h1>
         </header>
 
         <div class="row">
 
           <div class="medium-8 columns">
             <article class="rte">
-              <?php the_content(); ?>
+              <?php the_content() ?>
             </article>
           </div>
 
@@ -34,27 +33,26 @@ the_post();
                 <header>
                   <h3>Contact details</h3>
                 </header>
-                
+
                 <?php if (get_field('address')) : ?>
-                  <address><?php the_field('address'); ?></address>
+                  <address><?php the_field('address') ?></address>
                 <?php endif; ?>
 
                 <?php if (get_field('phone_number')) : ?>
-                  <a class="contact-link phone"><?php the_field('phone_number'); ?></a>
+                  <a class="contact-link phone"><?php the_field('phone_number') ?></a>
                 <?php endif; ?>
-                
+
                 <?php if (get_field('email_address')) : ?>
-                  <a class="contact-link email"><?php the_field('email_address'); ?></a>
+                  <a class="contact-link email"><?php the_field('email_address') ?></a>
                 <?php endif; ?>
 
               </div>
 
-            <?php endif; ?>
+            <?php endif ?>
 
           </aside>
 
         </div>
-
       </div>
     </div>
   </section>
