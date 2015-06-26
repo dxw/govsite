a3e77f3d13059ce8eed91153a6d9f0783781948a
Nina Regli <nina@dxw.com>
2015-06-26 15:52:11 +0100

    fix(front-page): fix image loop on front page

--- templates/partials/image-feature.php
+++ templates/partials/image-feature.php
@@ -1,15 +1,17 @@
-<?php if(!($the_image = get_field('image_' . $img))) { break; } ?>
+<?php if($the_image = get_field('image_' . $img)) :  ?>
 
-<li>
-  <article>
-    <header>
-      <h3><?php the_field('title_' . $img); ?></h3>
-    </header>
-    <a class="image" href="<?php the_field('url_' . $img); ?>">
-      <img class="thumb" src="<?php echo $the_image['sizes']['full'] ?>" alt="<?php echo $the_image['alt']; ?>">
-    </a>
-    <p>
-      <?php the_field('description_' . $img); ?> <a href="<?php the_field('url_' . $img); ?>"><?php _e('Read more &raquo;', 'govsite') ?></a>
-    </p>
-  </article>
-</li>
+  <li>
+    <article>
+      <header>
+        <h3><?php the_field('title_' . $img); ?></h3>
+      </header>
+      <a class="image" href="<?php the_field('url_' . $img); ?>">
+        <img class="thumb" src="<?php echo $the_image['sizes']['full'] ?>" alt="<?php echo $the_image['alt']; ?>">
+      </a>
+      <p>
+        <?php the_field('description_' . $img); ?> <a href="<?php the_field('url_' . $img); ?>"><?php _e('Read more &raquo;', 'govsite') ?></a>
+      </p>
+    </article>
+  </li>
+
+<?php endif ?>
