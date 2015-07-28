2130e68c7569f0cc5f42bf7a1620f883afdc8211
Adam Onishi <adam@dxw.com>
2015-07-28 12:28:25 +0100

    fix(admin): Fix admin JS and commit other compiled files

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,27 +2,28 @@ jQuery(function ($) {
     'use strict';
 
     // Logo
+    var logoSetting = $('input[name="logo-setting"]');
 
-    var logoSetting = $('input[name="logo-setting"]')
+    if( logoSetting.length ) {
+      var displayCurrentLogo = function () {
+          // Avoid XSS:
+          // - By using document.createElement instead of $().html('<img src="'+xyz+'">)
+          // - By checking the URL starts with / or https?://
 
-    var displayCurrentLogo = function () {
-        // Avoid XSS:
-        // - By using document.createElement instead of $().html('<img src="'+xyz+'">)
-        // - By checking the URL starts with / or https?://
+          if (logoSetting.val().match(/^(\/|https?:\/\/)/)) {
+              var img = document.createElement('img');
+              $(img).attr('src', logoSetting.val());
+              $(img).attr('style', 'max-width: 100%');
+              $('#logo-img').html(img);
+          }
+      }
 
-        if (logoSetting.val().match(/^(\/|https?:\/\/)/)) {
-            var img = document.createElement('img')
-            $(img).attr('src', logoSetting.val())
-            $(img).attr('style', 'max-width: 100%')
-            $('#logo-img').html(img)
-        }
+      logoSetting.after('<div id="logo-img" style="width: 260px; margin-top: 20px;"></div>');
+      logoSetting.after('<button id="logo-select-button">Select image</button>');
+      logoSetting.addClass('hidden');
+
+      displayCurrentLogo();
     }
-
-    logoSetting.after('<div id="logo-img" style="width: 260px; margin-top: 20px;"></div>')
-    logoSetting.after('<button id="logo-select-button">Select image</button>')
-    logoSetting.addClass('hidden')
-
-    displayCurrentLogo()
 
     // http://wordpress.stackexchange.com/questions/172383/how-to-get-an-image-url-from-media-library/172384
 
@@ -30,13 +31,13 @@ jQuery(function ($) {
 
     // Build the choose from library frame.
     $(document).on("click", "#logo-select-button", function(event) {
-        event.preventDefault()
+        event.preventDefault();
 
-        var id = $(this).attr('id')
+        var id = $(this).attr('id');
 
         if (frame) {
-            frame.open()
-            return
+            frame.open();
+            return;
         }
 
         // Create the media frame.
@@ -45,17 +46,17 @@ jQuery(function ($) {
             library: {
                 type: 'image'
             },
-        })
+        });
 
         // When an image is selected, run a callback.
         frame.on( 'select', function() {
             // Grab the selected attachment.
-            var attachment = frame.state().get('selection').first().toJSON()
-            logoSetting.val(attachment.url)
-            displayCurrentLogo()
+            var attachment = frame.state().get('selection').first().toJSON();
+            logoSetting.val(attachment.url);
+            displayCurrentLogo();
         })
 
-        frame.open()
+        frame.open();
     })
 
-})
+});
